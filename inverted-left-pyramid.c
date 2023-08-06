afef25fd5cea83c5455c3aa0637eb57ceb5f4e35
abelraju <abelraju12@gmail.com>
2023-08-06 15:15:26 +0530

    Adding folder

--- inverted-left-pyramid.c
+++ inverted-left-pyramid.c
@@ -4,11 +4,15 @@ int main(){
     int num;
     char ch[100];
     scanf("%d", &num);
-    for(int i = 0; i <= num; i++){
-        for(int j = 0; j <= num; j++){
-            if(j > i){
-                printf("* ");
-            }
+    for(int i = 1; i <= num; i++){
+        for(int j = 1; j <= num - i + 1; j++){
+            // if(i == 1 || j == 1 || j == num - i + 1){
+            //     printf("* ");
+            // }
+            // else{
+            //     printf("  ");
+            // }
+            printf("* ");
         }
         printf("\n");
     }
