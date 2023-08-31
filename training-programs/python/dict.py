3570eb1c566e7e7f0a2f18b560bf82d91542fa63
abelraju <abelraju12@gmail.com>
2023-08-31 09:27:00 +0530

    more collections

---
+++ training-programs/python/dict.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+d = defaultdict(list)
+# d['python'].append('awesome')
+# d['python'].append('awesomer')
+# d['python'].append('awesomest')
+# print(d)
+input_n, input_m = map(int, input().split())
+for i in range(input_n):
+    ans1 = input()
+    d[ans1].append(i+1)
+print(d)
+for j in range(input_m):
+    ans2 = input()
+    if ans2 in d:
+        print(*d[ans2])
+    else:
+        print(-1)
